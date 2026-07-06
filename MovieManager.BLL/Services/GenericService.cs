6f823ad8d08f516ad79c28450bd564a684adaf85
Davide <davide.barbieri1@outlook.it>
2026-07-06 16:51:31 +0200

    Fix: Interfaccia IGenericService

--- MovieManager.BLL/Services/GenericService.cs
+++ MovieManager.BLL/Services/GenericService.cs
@@ -1,42 +1,44 @@
-﻿
-using AutoMapper;
+﻿using AutoMapper;
 using MovieManager.BLL.Services.Interfaces;
 using MovieManager.DAL.Repositories.Interfaces;
 
-namespace MovieManager.BLL.Services
-{
-    public class GenericService<TModel, TEntity> : IGenericService<TModel>
+public class GenericService<TModel, TEntity> : IGenericService<TModel>
     where TModel : class
     where TEntity : class
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IGenericRepository<TEntity> _repository;
+    private readonly IMapper _mapper;
+
+    public GenericService(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IRepository<TEntity> _repository;
-        private readonly IMapper _mapper;
-
-        public Task<TModel> CreateAsync(TModel model, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IReadOnlyList<TModel>> GetAllAsync(CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T?> GetByIdAsync<T>(int id, CancellationToken cancellationToken = default) where T : class
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> UpdateAsync(TModel model, CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+        _repository = _unitOfWork.Repository<TEntity>();
     }
 
+    public Task<TModel?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<TModel>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TModel> CreateAsync(TModel model, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateAsync(TModel model, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
--- MovieManager.BLL/Services/Interfaces/IGenericService.cs
+++ MovieManager.BLL/Services/Interfaces/IGenericService.cs
@@ -1,12 +1,17 @@
 ﻿namespace MovieManager.BLL.Services.Interfaces
 
 {
-    public interface IGenericService <TModel> where TModel : class
+    public interface IGenericService<TModel>
     {
-        Task<T?> GetByIdAsync<T>(int id, CancellationToken cancellationToken = default) where T : class;
+        Task<TModel?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+
         Task<IReadOnlyList<TModel>> GetAllAsync(CancellationToken cancellationToken = default);
+
         Task<TModel> CreateAsync(TModel model, CancellationToken cancellationToken = default);
+
         Task<bool> UpdateAsync(TModel model, CancellationToken cancellationToken = default);
+
         Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
     }
+
 }
