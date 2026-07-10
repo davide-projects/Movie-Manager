7e53831c09141f4d926a32cf0f3a2f83a8266c9e
Davide <davide.barbieri1@outlook.it>
2026-07-10 16:44:19 +0200

    Update: Inziato MovieController (To do: Creare Put e Delete)

---
+++ MovieManager.PL.API/Controllers/MovieController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManager.BLL.Models;
+using MovieManager.BLL.Services.Interfaces;
+
+namespace MovieManager.PL.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class MovieController : ControllerBase
+    {
+        private readonly IGenericService<MovieModel> _movieService;
+
+        public MovieController(IGenericService<MovieModel> movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieModel>>> GetAllAsync()
+        {
+            var movies = await _movieService.GetAllAsync();
+            return Ok(movies);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieModel>> GetByIdAsync(int id)
+        {
+            var movie = await _movieService.GetByIdAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+        [HttpPost]
+        public async Task<ActionResult<MovieModel>> CreateAsync([FromBody] MovieModel model)
+        {
+            var created = await _movieService.CreateAsync(model);
+            return CreatedAtAction(nameof(GetByIdAsync), new { id = created.Id }, created);
+        }
+    }
+}
